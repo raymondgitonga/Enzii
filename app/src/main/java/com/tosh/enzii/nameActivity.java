9c481679676abf3759f7291503af80b3480f1314
raymond gitonga <raytosh95@gmail.com>
2019-05-09 14:55:57 +0300

    create activity to get name of users

--- app/src/main/java/com/tosh/enzii/nameActivity.java
+++ app/src/main/java/com/tosh/enzii/nameActivity.java
@@ -3,12 +3,11 @@ package com.tosh.enzii;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class nameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_name);
     }
 }
