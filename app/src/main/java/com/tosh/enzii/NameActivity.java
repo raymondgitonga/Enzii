eedacc60a4524649562825414ce313deb4913427
raymond gitonga <raytosh95@gmail.com>
2019-05-09 16:24:48 +0300

    create toast that displays welcome message when the name button is pressed

--- app/src/main/java/com/tosh/enzii/NameActivity.java
+++ app/src/main/java/com/tosh/enzii/NameActivity.java
@@ -2,12 +2,25 @@ package com.tosh.enzii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NameActivity extends AppCompatActivity {
+    private Button mNameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+
+        mNameButton = (Button) findViewById(R.id.nameButton);
+
+        mNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(NameActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
