5546248f0e4ae44950cb583f88e8a346c0766052
raymond gitonga <raytosh95@gmail.com>
2019-05-09 18:47:44 +0300

    create activity that will display the name of the the user and add the text view

--- app/src/main/java/com/tosh/enzii/NameActivity.java
+++ app/src/main/java/com/tosh/enzii/NameActivity.java
@@ -5,22 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class NameActivity extends AppCompatActivity {
     private Button mNameButton;
-
+    private EditText mEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
 
         mNameButton = (Button) findViewById(R.id.nameButton);
+        mEditText = (EditText) findViewById(R.id.editText);
 
         mNameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = mEditText.getText().toString();
                 Intent intent = new Intent(NameActivity.this, SelectionActivity.class);
+                intent.putExtra("name", name);
                 startActivity(intent);
                 Toast.makeText(NameActivity.this, "Welcome", Toast.LENGTH_LONG).show();
             }
--- app/src/main/java/com/tosh/enzii/SelectionActivity.java
+++ app/src/main/java/com/tosh/enzii/SelectionActivity.java
@@ -1,13 +1,23 @@
 package com.tosh.enzii;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SelectionActivity extends AppCompatActivity {
+
+    private TextView nameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        nameView =(TextView) findViewById(R.id.nameView);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        nameView.setText("Welcome " + name +"!");
+
     }
 }
