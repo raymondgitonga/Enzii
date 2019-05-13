ccdec5a4a255022ed83bdae344d3dc8d7cc1a79b
raymond gitonga <raytosh95@gmail.com>
2019-05-13 03:53:52 +0300

    create toast for when each headline is clicked

--- app/src/main/java/com/tosh/enzii/SelectionActivity.java
+++ app/src/main/java/com/tosh/enzii/SelectionActivity.java
@@ -3,14 +3,17 @@ package com.tosh.enzii;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SelectionActivity extends AppCompatActivity {
 
     private TextView nameView;
-    private ListView newsView;
+    private ListView mNewsView;
     private String[] headlines = new String[] {"Petitioners: Stop Huduma Namba registration",
             "South Africa demonstrated elections can be peaceful",
             "Ignore summons at your own peril, Senate warns governors",
@@ -29,12 +32,18 @@ public class SelectionActivity extends AppCompatActivity {
 
 
         nameView =(TextView) findViewById(R.id.nameView);
-        newsView = (ListView) findViewById(R.id.newsView);
+        mNewsView = (ListView) findViewById(R.id.newsView);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_2, headlines);
-        newsView.setAdapter(adapter);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, headlines);
+        mNewsView.setAdapter(adapter);
 
-        newsView.setOn
+        mNewsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String headlines =((TextView)view).getText().toString();
+                Toast.makeText(SelectionActivity.this, "Preparing your news", Toast.LENGTH_LONG).show();
+            }
+        });
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         nameView.setText("Welcome " + name +"!");
