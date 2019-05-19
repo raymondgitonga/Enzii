b9d747847a9428ac95557f6d01d5d366937efaeb
raymond gitonga <raytosh95@gmail.com>
2019-05-19 15:33:06 +0300

    create a search fiels for searching any news

--- app/src/main/java/com/tosh/enzii/SelectionActivity.java
+++ app/src/main/java/com/tosh/enzii/SelectionActivity.java
@@ -19,38 +19,17 @@ import butterknife.ButterKnife;
 public class SelectionActivity extends AppCompatActivity {
 
     @BindView(R.id.nameView) TextView nameView;
-    @BindView(R.id.newsView) ListView mNewsView;
-    private String[] headlines = new String[] {"Petitioners: Stop Huduma Namba registration",
-            "South Africa demonstrated elections can be peaceful",
-            "Ignore summons at your own peril, Senate warns governors",
-            "Inside Raila 2022 game plan as party lines up rallies",
-            "Senator in Sh400m gold scam involving UAE royal family",
-            "Fire erupts again in Mt Kenya Forest in suspected arson",
-            "Tanzanian arrested for selling daughter for body parts",
-            "Julius Malema fires revolutionary signal in South Africa's election results",
-            "Local banks take Sh8 billion hit in KQ shares",
-            "The 10 Best Travel Apps for Exploring the World"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
 
-//        Typeface mediumFont = Typeface.createFromAsset(getAssets(), "fonts/Medium.ttf");
-//        nameView.setTypeface(mediumFont);
 
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, headlines);
-        mNewsView.setAdapter(adapter);
 
-        mNewsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String headlines =((TextView)view).getText().toString();
-                Toast.makeText(SelectionActivity.this, "Preparing your news", Toast.LENGTH_LONG).show();
-            }
-        });
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         nameView.setText("Welcome " + name +"!");
