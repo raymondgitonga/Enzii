345387d07c7fe0c1d58b002d13ee5539bb913532
raymond gitonga <raytosh95@gmail.com>
2019-05-19 23:14:09 +0300

    create adapter that will handle the api request from the server

--- app/src/main/java/com/tosh/enzii/SelectionActivity.java
+++ app/src/main/java/com/tosh/enzii/SelectionActivity.java
@@ -1,26 +1,17 @@
 package com.tosh.enzii;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.graphics.Typeface;
+
 
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SelectionActivity extends AppCompatActivity {
+    public
 
     @BindView(R.id.nameView) TextView nameView;
 
--- app/src/main/java/com/tosh/enzii/models/Article.java
+++ app/src/main/java/com/tosh/enzii/models/Article.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 
-class Article {
+public class Article {
 
     @SerializedName("Source")
     @Expose
