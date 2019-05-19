f885e615536a86e9de3d97a7f6f2bab6a91a5153
raymond gitonga <raytosh95@gmail.com>
2019-05-19 18:20:20 +0300

    grant app permission to access internet from the manifest

--- app/src/main/java/com/tosh/enzii/SelectionActivity.java
+++ app/src/main/java/com/tosh/enzii/SelectionActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +23,10 @@ import butterknife.ButterKnife;
 public class SelectionActivity extends AppCompatActivity {
 
     @BindView(R.id.nameView) TextView nameView;
+
+    @BindView(R.id.search_field) EditText searchField;
+    @BindView(R.id.search_btn) ImageButton searchButton;
+    @BindView(R.id.result_list) RecyclerView resultList;
 
 
     @Override
@@ -34,5 +42,8 @@ public class SelectionActivity extends AppCompatActivity {
         String name = intent.getStringExtra("name");
         nameView.setText("Welcome " + name +"!");
 
+
     }
+
+
 }
